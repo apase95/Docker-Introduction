71870d105e7823db35f2d93088fda9bab8de9724
apase95 <noobyhandsome@gmail.com>
2026-03-18 22:16:43 +0700

    [PRACTICE]: Docker First Step

--- docker-project-1/src/app.service.ts
+++ docker-project-1/src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const message = process.env.WELCOME_MESSAGE;
+    return message
+      ? message
+      : 'Hello from localhost, no Welcome Message env var';
   }
 }
